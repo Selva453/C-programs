a79bd3774ed0e055b1b39d7fe79a201ad4a96fb7
Selva453 <87667459+Selva453@users.noreply.github.com>
2021-07-22 01:00:34 +0530

    Update bitwise-macro.c

--- bitwise-macro.c
+++ bitwise-macro.c
@@ -1,20 +1,29 @@
-// CPP program to check if k-th bit
-// of a given number is set or not
-#include <iostream>
-using namespace std;
+/******************************************************************************
 
-void isKthBitSet(int n, int k)
-{
-	if (n & (1 << (k - 1)))
-		cout << "SET";
-	else
-		cout << "NOT SET";
-}
+                           bitwise using macro
 
-// Driver code
-int main()
-{
-	int n = 5, k = 1;
-	isKthBitSet(n, k);
-	return 0;
+*******************************************************************************/
+
+#include <stdio.h>
+#define SET_BIT(n,k) n|(1<<(k-1))
+#define CLEAR_BIT(n,k) n&(~(1<<(k-1)))
+#define TOGGLE(n,k) n^(1<<(k-1))
+#define IS_BIT_SET(n,k)  ((n&(1<<(k-1))) > 0) ? "YES" : "NO"
+
+int main (){
+    int n=0, k =0;
+    
+    printf ("\nEnter any no...\n");
+    scanf ("%d", &n);
+    
+    printf ("\nEnter position..\n");
+    scanf ("%d", &k);
+    
+    /*printf ("\nNo after setting the bit : %d", (SET_BIT(n,k)));
+    printf ("\nNo after clearing the bit : %d", (CLEAR_BIT(n,k)));
+    printf ("\nNo after Toggling the bit : %d", (TOGGLE(n,k)));*/
+    printf ("\nBit at %d postion is SET : %s", k,(IS_BIT_SET(n,k)));
+    
+
+    return 0;
 }
